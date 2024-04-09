cab63b9081e52b644dc714b4f7ae59ac1a6da2e6
HakimHC <ahmedh90111@gmail.com>
2024-04-09 23:29:21 +0200

    Downloading wordpress files

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -4,6 +4,8 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
+wp core download --allow-root
+
 wp config create \
 	 --dbname=$DB_DATABASE \
 	  --dbuser=$DB_USER \

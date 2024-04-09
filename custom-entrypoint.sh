b4689df2306fd1663e93dcfb76e37546dd072970
HakimHC <ahmedh90111@gmail.com>
2024-04-10 00:02:48 +0200

    Last test

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -16,8 +16,8 @@ wp config create \
 
 echo "define('FORCE_SSL_ADMIN', true);" >> wp-config.php
 echo "define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);" >> wp-config.php
-echo "$_SERVER['HTTPS'] = 'on';" >> wp-config.php
-echo "$_SERVER['SERVER_PORT'] = 443;" >> wp-config.php
+# echo "$_SERVER['HTTPS'] = 'on';" >> wp-config.php
+# echo "$_SERVER['SERVER_PORT'] = 443;" >> wp-config.php
 
 exec apache2-foreground
 

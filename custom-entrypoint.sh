52f8a3ab55b799859bf7c005288b4ef6298f371b
HakimHC <ahmedh90111@gmail.com>
2024-04-09 23:56:57 +0200

    Adding tls related stuff

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -4,11 +4,6 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-echo "host: $DB_HOST"
-echo "user: $DB_USER"
-echo "pass: $DB_PASSWORD"
-echo "db: $DB_DATABASE"
-
 wp core download --allow-root
 
 wp config create \
@@ -19,10 +14,10 @@ wp config create \
 	  --allow-root
 
 
-# echo "define('FORCE_SSL_ADMIN', true);" >> wp-config.php
-# echo "define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);" >> wp-config.php
-# echo "$_SERVER['HTTPS'] = 'on';" >> wp-config.php
-# echo "$_SERVER['SERVER_PORT'] = 443;" >> wp-config.php
+echo "define('FORCE_SSL_ADMIN', true);" >> wp-config.php
+echo "define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);" >> wp-config.php
+echo "$_SERVER['HTTPS'] = 'on';" >> wp-config.php
+echo "$_SERVER['SERVER_PORT'] = 443;" >> wp-config.php
 
 exec apache2-foreground
 

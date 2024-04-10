ac9fac063d9bae04a772750a813f66ea9bff94ce
HakimHC <ahmedh90111@gmail.com>
2024-04-10 09:24:57 +0200

    Trying out manually inserting wp-config

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -6,18 +6,15 @@ mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root
 
-wp config create \
-	 --dbname=$DB_DATABASE \
-	  --dbuser=$DB_USER \
-	  --dbpass=$DB_PASSWORD \
-	  --dbhost=$DB_HOST \
-	  --allow-root
+cp /wp-config.php .
 
+# wp config create \
+# 	 --dbname=$DB_DATABASE \
+# 	  --dbuser=$DB_USER \
+# 	  --dbpass=$DB_PASSWORD \
+# 	  --dbhost=$DB_HOST \
+# 	  --allow-root
 
-echo "define('FORCE_SSL_ADMIN', true);" >> wp-config.php
-echo "define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);" >> wp-config.php
-# echo "$_SERVER['HTTPS'] = 'on';" >> wp-config.php
-# echo "$_SERVER['SERVER_PORT'] = 443;" >> wp-config.php
 
 exec apache2-foreground
 

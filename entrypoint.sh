e36c3bd0bd9ee067507d7c2450e8cd730ff6b7e1
HakimHC <ahmedh90111@gmail.com>
2024-04-12 20:18:24 +0200

    Adding more permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@ wp theme activate --allow-root twentytwentythree
 wp plugin install --allow-root --activate simply-static
 wp plugin install --allow-root --activate /simply-static-pro.zip
 
+chmod -R 755 /var/www/html/wp-content/uploads
+
 exec apache2-foreground

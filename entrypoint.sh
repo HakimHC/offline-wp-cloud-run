abb4a8464e52241844dce92bc407b5a1deb13f0f
HakimHC <ahmedh90111@gmail.com>
2024-04-12 21:14:18 +0200

    more permissions...

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,7 @@ wp plugin install --allow-root --activate /simply-static-pro.zip
 chmod -R 755 /var/www/html/wp-content/uploads
 chown -R nobody:nogroup /var/www/html/wp-content/uploads
 
+chmod -R 777 /var/www/html/wp-content/uploads/simply-static
+chown -R nobody:nogroup /var/www/html/wp-content/uploads
+
 exec apache2-foreground

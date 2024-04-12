5bbb3b84bdf3d3906795f6538c54d2c1b13377ef
HakimHC <ahmedh90111@gmail.com>
2024-04-12 21:15:48 +0200

    more permissions... to www-data

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ chmod -R 755 /var/www/html/wp-content/uploads
 chown -R nobody:nogroup /var/www/html/wp-content/uploads
 
 chmod -R 777 /var/www/html/wp-content/uploads/simply-static
-chown -R nobody:nogroup /var/www/html/wp-content/uploads
+chown -R www-data:www-data /var/www/html/wp-content/uploads/simply-static/
 
 exec apache2-foreground

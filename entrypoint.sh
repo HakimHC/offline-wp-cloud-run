f7127ef48aa0405dfce3489b35bdcd8b1be51b33
HakimHC <ahmedh90111@gmail.com>
2024-04-14 13:13:46 +0200

    Adding token for authorization of invoking cloud function

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,24 @@
 #! /bin/bash
 
+metadata_token_url="http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token"
+
+get_auth_token() {
+	curl \
+	-H "Metadata-Flavor: Google" \
+	$metadata_token_url | tr -d "\n"
+}
+
 get_cloud_run_domain_name() {
 	curl \
 	-d "{\"service_name\": \"$SERVICE_NAME\"}" \
 	-H "Content-Type: application/json" \
+	-H "Authorization: Bearer $(get_auth_token)" \
 	"$DOMAIN_FUNCTION_URL" | jq -r ".service_url" | tr -d "\n"
 }
 
 domain_name="$(get_cloud_run_domain_name)"
+
+# CONDITION HERE
 
 wp core install --allow-root --url=$domain_name --title="Altostratus Wordpress Blog" \
 	--admin_user="admin" \
@@ -21,13 +32,6 @@ chmod -R 755 /var/www/html/wp-content/uploads/simply-static
 chown -R www-data:www-data /var/www/html/wp-content/uploads/simply-static/
 
 echo "add_filter( 'ssp_single_auto_export','__return_true' );" >> wp-includes/functions.php
-
-# echo "define( 'SSP_GITHUB', [
-# 	'github-personal-access-token' => '$SSP_GITHUB_TOKEN',
-# 	'github-user'                  => '$SSP_GITHUB_USER',
-# 	'github-repository'            => '$SSP_GITHUB_REPO',
-# 	'github-branch'                => '$SSP_GITHUB_BRANCH'
-# ] );" >> wp-config.php
 
 echo "define( 'SSP_GITHUB', [
 	'github-personal-access-token' => '$SSP_GITHUB_TOKEN',

ad55c4d1e8d5a9c73de810fb11b2b55efefbbf0d
HakimHC <ahmedh90111@gmail.com>
2024-04-14 22:09:08 +0200

    Added placeholders for vpc connector and sa

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,35 +3,35 @@
 metadata_token_url="http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=$DOMAIN_FUNCTION_URL"
 
 get_auth_token() {
-	curl \
-	-H "Metadata-Flavor: Google" \
-	$metadata_token_url
+        curl \
+        -H "Metadata-Flavor: Google" \
+        $metadata_token_url
 }
 
 echo "TOKEN: $(get_auth_token)"
 
 get_cloud_run_domain_name() {
-	curl \
-	-d "{\"service_name\": \"$SERVICE_NAME\"}" \
-	-H "Content-Type: application/json" \
-	-H "Authorization: Bearer $(get_auth_token)" \
-	"$DOMAIN_FUNCTION_URL" | jq -r ".service_url" | tr -d "\n"
+        curl \
+        -d "{\"service_name\": \"$SERVICE_NAME\"}" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $(get_auth_token)" \
+        "$DOMAIN_FUNCTION_URL" | jq -r ".service_url" | tr -d "\n"
 }
 
 count=0
 while [ $count -lt 5 ]; do
-	domain_name=$(get_cloud_run_domain_name)
-	if [[ ! -z "$domain_name" ]]; then
-		break
-	fi
-	sleep 5
-	((count++))
+        domain_name=$(get_cloud_run_domain_name)
+        if [[ ! -z "$domain_name" ]]; then
+                break
+        fi
+        sleep 5
+        ((count++))
 done
 
 if [[ -z "$domain_name" ]]; then
-	echo "Failed to retrieve the service URL after 5 attempts."
-	domain_name="https://deploy-run-service-iwcc7qj55q-ew.a.run.app"
-	echo "Defaulting to $domain_name"
+        echo "Failed to retrieve the service URL after 5 attempts."
+        domain_name="https://deploy-run-service-iwcc7qj55q-ew.a.run.app"
+        echo "Defaulting to $domain_name"
 fi
 
 curl \
@@ -45,26 +45,28 @@ echo "DOMAIN FUNC: $DOMAIN_FUNCTION_URL"
 
 echo "DOMAIN NAME: $domain_name"
 
-# CONDITION HERE
+if ! wp core is-installed 2&gt;/dev/null; then
 
-wp core install --allow-root --url=$domain_name --title="Altostratus Wordpress Blog" \
-	--admin_user="admin" \
-	--admin_email="admin@bootcamp.altostratus.es"
+        wp core install --allow-root --url=$domain_name --title="Altostratus Wordpress Blog" \
+        --admin_user="admin" \
+        --admin_email="admin@bootcamp.altostratus.es"
 
-wp theme activate --allow-root twentytwentythree
-wp plugin install --allow-root --activate simply-static
-wp plugin install --allow-root --activate /simply-static-pro.zip
+        wp theme activate --allow-root twentytwentythree
+        wp plugin install --allow-root --activate simply-static
+        wp plugin install --allow-root --activate /simply-static-pro.zip
 
-chmod -R 755 /var/www/html/wp-content/uploads/simply-static
-chown -R www-data:www-data /var/www/html/wp-content/uploads/simply-static/
+        chmod -R 755 /var/www/html/wp-content/uploads/simply-static
+        chown -R www-data:www-data /var/www/html/wp-content/uploads/simply-static/
 
-echo "add_filter( 'ssp_single_auto_export','__return_true' );" >> wp-includes/functions.php
+        echo "add_filter( 'ssp_single_auto_export','__return_true' );" >> wp-includes/functions.php
 
-echo "function add_custom_http_header(\$r, \$url) {
-    \$r['headers']['Authorization'] = 'Bearer $(get_auth_token)';
-    return \$r;
-}
-add_filter('http_request_args', 'add_custom_http_header', 10, 2);" >> wp-config.php
+        echo "function add_custom_http_header(\$r, \$url) {
+                \$r['headers']['Authorization'] = 'Bearer $(get_auth_token)';
+                return \$r;
+        }
+        add_filter('http_request_args', 'add_custom_http_header', 10, 2);" >> wp-config.php
+
+fi
 
 
 exec apache2-foreground
